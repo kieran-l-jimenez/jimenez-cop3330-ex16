c720c2b5342a0e9edc53cc05a49f248310b44a34
Kieran Jimenez <kieran.l.jimenez@gmail.com>
2021-06-06 18:51:21 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Kieran Jimenez
+ */
+/*
+ *           "Computing Simple Interest" output
+ *What is your age? 15
+ *You are not old enough to legally drive.
+ *           Or
+ *What is your age? 35
+ *You are old enough to legally drive.
+ */
+public class App
+{
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        String result;
+        int drivingAge = 16;
+        System.out.print("What is your age? ");
+        int age = in.nextInt();
+
+        result = age >= drivingAge ? " " : " not ";
+
+        System.out.println("You are" + result +"old enough to legally drive.");
+    }
+
+}
